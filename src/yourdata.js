6fef8d80d26559f0c4501120dcf62e12e3e0ce24
devghai <devkghai@gmail.com>
2024-03-12 23:21:36 -0700

    update

--- src/yourdata.js
+++ src/yourdata.js
@@ -6,24 +6,24 @@ export default
         name :'devghai.',
         
         headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Dev.',
+                        'Hello. I am Dev.',
                         //Line 2 For Header
-                        'a current HS student',
+                        'a Junior at San Jose State University',
                         //Line 3 For Header
-                        'from California'
+                        ''
     ],
         //Contact Email
-        contactEmail:'devkghai@gmail.com',
+        contactEmail:'dev.ghai@sjsu.edu',
         // Add Your About Text Here
-        abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        abouttext: "I am currently a third year mathematics and computer science student at San Jose State. I enjoy learning new technolgoies and pushing the boundaries of what's possible in the fields of Data Science and Artificial Intelligence. I am currently working on mastering Set Theory, Propability Theory and Formal Langauges in order to craft high value technologies for ++.",
+        aboutImage:'https://unsplash.com/photos/bENplJMtdsw',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
        // Change Projects Here 
        projects:[
            {
             id: 1,//DO NOT CHANGE THIS (Please)😅
-            title:'Lemonade Stand', //Project Title - Add Your Project Title Here
+            title:'MacroNutrient Tracker', //Project Title - Add Your Project Title Here
              service:'Business', // Add Your Service Type Here
             //Project Image - Add Your Project Image Here
              imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
@@ -32,22 +32,22 @@ export default
             },
             {
                 id: 2,//DO NOT CHANGE THIS (Please)😅
-                title: 'Portfolio',
-                service: 'Source Code',
+                title: 'new2',
+                service: 'Code',
                 imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
                 url: 'http://chetanverma.com/'
             },
             { 
                 id: 3,//DO NOT CHANGE THIS (Please)😅
-                title: 'Kang',
-                service: 'iOS Development',
+                title: 'new1',
+                service: 'tech stack used*',
                 imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
                 url: 'http://chetanverma.com/'
             },
             {
                 id: 4,//DO NOT CHANGE THIS (Please)😅
-                title: 'Tic Tac Toe',
-                service: 'Artifical Intelligence',
+                title: 'Portfolio',
+                service: 'Source Code',
                 imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
                 url: 'http://chetanverma.com/'
            }
